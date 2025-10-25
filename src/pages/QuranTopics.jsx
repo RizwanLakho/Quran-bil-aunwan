1e4a2ff952b016bb479abe209e164e1ee2ad6aab
RizwanLakho <myofficeid192@gmail.com>
2025-10-25 21:24:00 +0500

    optimizing colors

--- src/pages/QuranTopics.jsx
+++ src/pages/QuranTopics.jsx
@@ -258,10 +258,10 @@ export default function QuranTopics() {
                     placeholder={t("search_topics")}
                     value={searchTerm}
                     onChange={(e) => setSearchTerm(e.target.value)}
-                    className="w-full px-4 py-2 md:py-2.5 pl-10 rounded-full border-2 border-white border-opacity-30 transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50 text-sm md:text-base bg-white bg-opacity-20 text-white placeholder-white placeholder-opacity-80 backdrop-blur-sm"
+                    className="w-full px-4 py-2 md:py-2.5 pl-10 rounded-full border-2 border-primary border-opacity-30 transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50 text-sm md:text-base bg-white bg-opacity-20 text-primary placeholder-primary placeholder-opacity-80 backdrop-blur-sm"
                   />
                   <svg
-                    className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 md:w-5 md:h-5 text-white opacity-70"
+                    className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 md:w-5 md:h-5 text-primary opacity-70"
                     fill="currentColor"
                     viewBox="0 0 20 20"
                   >
